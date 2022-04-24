9a7f93154bb218a343519f8dcbea466e0abe0e3c
Qinja <qinjiawang@tencent.com>
2022-04-24 10:49:52 +0800

    modify InitHash accessibility level

--- Assets/7.OcclusionCulling/Debug/PreZDebugger.cs
+++ Assets/7.OcclusionCulling/Debug/PreZDebugger.cs
@@ -6,8 +6,8 @@ namespace OcclusionCulling
 	{
 		public Camera DCamera;
 		public Material DMaterial;
-		public int InitHash;
 
+		private int initHash;
 		private Bounds proxyBounds;
 		private ComputeBuffer debugBuffer;
 		void Start()
@@ -15,17 +15,14 @@ namespace OcclusionCulling
 			proxyBounds = new Bounds(Vector3.zero, 1000.0f * Vector3.one);
 			debugBuffer = new ComputeBuffer(1, sizeof(uint));
 			Graphics.SetRandomWriteTarget(1, debugBuffer);
-			if (InitHash == 0)
-			{
-				InitHash = Random.Range(0x12341234, 0x67896789);
-			}
-			debugBuffer.SetData(new uint[1] { (uint)InitHash });
+			initHash = Random.Range(0x12341234, 0x67896789);
+			debugBuffer.SetData(new uint[1] { (uint)initHash });
 		}
 		void Update()
 		{
 			uint[] debugData = new uint[1];
 			debugBuffer.GetData(debugData);
-			if (debugData[0] == InitHash)
+			if (debugData[0] == initHash)
 			{
 				Debug.Log("EarlyZ is vaild");
 			}
@@ -35,9 +32,9 @@ namespace OcclusionCulling
 				Debug.LogError("EarlyZ is invaild");
 			}
 			int randomHash = Random.Range(0x12341234, 0x67896789);
-			if (randomHash == InitHash)
+			if (randomHash == initHash)
 			{
-				randomHash = InitHash + 0x00120012;
+				randomHash = initHash + 0x00120012;
 			}
 			DMaterial.SetInt("_Hash", randomHash);
 			Graphics.DrawProcedural(DMaterial, proxyBounds, MeshTopology.Triangles, 3, 1, DCamera);
